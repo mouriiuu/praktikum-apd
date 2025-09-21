268ad3b79937d5f7172fefcb9a8a3e93109a244a
mouriiuu <mutiarahmah020107@gmail.com>
2025-09-21 11:53:39 +0800

    first commit

---
+++ posttest/post-test-apd-2/Mutia-Rahmah-2509106040.py
@@ -0,0 +1,35 @@
+# List berisi int, float, string (numerik), dan boolean
+daftar_data = [10, 2.5, "3.14", True]
+
+print("=== SEBELUM DIUBAH ===")
+print("1.", "nilai =", daftar_data[0], "| tipe =", type(daftar_data[0]))
+print("2.", "nilai =", daftar_data[1], "| tipe =", type(daftar_data[1]))
+print("3.", "nilai =", daftar_data[2], "| tipe =", type(daftar_data[2]))
+print("4.", "nilai =", daftar_data[3], "| tipe =", type(daftar_data[3]))
+
+
+# penjumlahan integer dan float
+hasil_penjumlahan_1 = daftar_data[0] + daftar_data[1]
+print ("hasil penjumlahan = ",hasil_penjumlahan_1)
+
+# Konversi tipe (langsung)
+# int   -> str
+# float -> int
+# str   -> float
+# bool  -> int
+nilai0_baru = str(daftar_data[0])    # 10 -> "10"
+nilai1_baru = int(daftar_data[1])    # 2.5 -> 2
+nilai2_baru = float(daftar_data[2])  # "3.14" -> 3.14
+nilai3_baru = int(daftar_data[3])    # True -> 1
+
+daftar_setelah = [nilai0_baru, nilai1_baru, nilai2_baru, nilai3_baru]
+
+print("\n=== SESUDAH DIUBAH ===")
+print("1.", "nilai =", daftar_setelah[0], "| tipe =", type(daftar_setelah[0]))
+print("2.", "nilai =", daftar_setelah[1], "| tipe =", type(daftar_setelah[1]))
+print("3.", "nilai =", daftar_setelah[2], "| tipe =", type(daftar_setelah[2]))
+print("4.", "nilai =", daftar_setelah[3], "| tipe =", type(daftar_setelah[3]))
+
+# penjumlahan integer dan float
+hasil_penjumlahan_2 = daftar_setelah[2] + daftar_setelah[1]
+print ("hasil penjumlahan = ",hasil_penjumlahan_2)
